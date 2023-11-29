d87b33a291f1cdfb0fdab8786f85de6b3794772a
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2023-11-29 19:44:00 +0200

    fixing file structure & start in the nav bar

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navclose = document.getElementById('nav-close')
+
+
+/*=============== validate if constant exists ===============*/
+if (navToggle){
+    navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+    })
+}
+
+
+/*=============== validate if constant exists ===============*/
+if (navclose){
+    navclose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+    })
+}   
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD BLUR TO HEADER ===============*/
+
+
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
