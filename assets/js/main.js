7068c8696a84789fad6e0d9ae144c55ff4c045d4
Ahemd Abdeen <ahmedwalleed15@gmail.com>
2023-12-22 02:20:11 +0200

    adding Scroll Up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,8 +60,13 @@ const sendEmail = (e)=>{
 content_Form.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
-
+ const scrollUp = ()=>{
+    const scrollUp = document.getElementById('scroll-up');
+    console.log(scrollUp);
+    this.scrollY >=350 ? scrollUp.classList.add('show-scroll'):
+    scrollUp.classList.remove('show-scroll')
+ }
+window.addEventListener('scroll',scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
